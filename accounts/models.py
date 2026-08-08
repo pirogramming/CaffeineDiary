57c9ad7ae3668781ea04dcdecd7644b1835e935a
ethan0587 <leesch587@naver.com>
2026-08-08 19:30:41 +0900

    feat:UserProfile 모델 작성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+    body_weight_kg = models.FloatField(null=True, blank=True)      
+    target_sleeptime = models.TimeField(null=True, blank=True)         
+    profile_image = models.ImageField(upload_to="profile_images/", null=True, blank=True) 
+
+    def __str__(self):
+        return f"{self.user.username} UserProfile"
