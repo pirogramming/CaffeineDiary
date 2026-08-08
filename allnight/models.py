85d610818202cc1bc79adb6c899185d7f4c3a706
ethan0587 <leesch587@naver.com>
2026-08-08 20:16:14 +0900

    feat: AllNightSession에 created at 추가

--- allnight/models.py
+++ allnight/models.py
@@ -7,11 +7,12 @@ class AllNightSession(models.Model):
         on_delete=models.CASCADE,
         related_name="allnight_sessions",
     )
-    status = models.CharField(max_length=20, default="active")  # 세션상태 
+    status = models.CharField(max_length=20, default="active")  # 세션상태: NULL/ACTIVE/ABORTED/COMPLETED 
     started_at = models.DateTimeField()                      # 시작 날짜
     ended_at = models.DateTimeField(null=True, blank=True)  # 종료 날짜
     target_time = models.DateTimeField(default=False)       # 목표 종료 시각
     is_active = models.BooleanField(default=False)  # 밤샘모드 여부
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
