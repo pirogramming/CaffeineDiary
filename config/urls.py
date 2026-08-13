e36e622e42f3bbfdf55f6674fb6500045cfdafce
hyunnminn <65217613+StormtroopeR2727@users.noreply.github.com>
2026-08-13 17:43:18 +0900

    chore: API확인용 문서/테스트 프론트 페이지 추가

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.http import JsonResponse
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve as static_serve
 
 
 def home(request):
@@ -37,3 +39,13 @@ urlpatterns = [
     path('auth/social/', include('allauth.urls')),
     path('', include('diary.urls')),
 ]
+
+if settings.DEBUG:
+    # 개발용 테스트 프론트(testpages/)를 같은 origin에서 서빙 - 세션/CSRF 쿠키 공유 목적
+    urlpatterns += [
+        re_path(
+            r"^testpages/(?P<path>.*)$",
+            static_serve,
+            {"document_root": settings.BASE_DIR / "testpages"},
+        ),
+    ]
