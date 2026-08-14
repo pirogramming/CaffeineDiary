251892d2528948f3fd452a39bfdab10c0f005e54
bigdevlight <bigdevlight@gmail.com>
2026-08-15 07:45:05 +0900

    feat: 마이페이지 마크업

--- diary/urls.py
+++ diary/urls.py
@@ -8,7 +8,7 @@ config/urls.py에서 path("", include("diary.urls"))로 루트에 마운트된�
 """
 
 from django.urls import path
-
+from django.views.generic import TemplateView
 from . import views
 
 app_name = "diary"
@@ -48,4 +48,5 @@ urlpatterns = [
 
     # feed url 임시로추가
     path("feed/", views.FeedView.as_view(), name="feed"),  
+    path("mypage/", TemplateView.as_view(template_name="diary/mypage.html"), name="mypage"),
 ]
