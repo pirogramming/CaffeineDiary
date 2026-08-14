ae973303d20d05743d1778734ab2fff302383418
bigdevlight <bigdevlight@gmail.com>
2026-08-15 05:00:49 +0900

    [feat] 즐겨찾기 페이지, 음료 수정 오버레이 마크업

--- diary/urls.py
+++ diary/urls.py
@@ -8,7 +8,7 @@ config/urls.py에서 path("", include("diary.urls"))로 루트에 마운트된�
 """
 
 from django.urls import path
-
+from django.views.generic import TemplateView
 from . import views
 
 app_name = "diary"
@@ -37,4 +37,6 @@ urlpatterns = [
 
     # feed url 임시로추가
     path("feed/", views.FeedView.as_view(), name="feed"),  
+
+    path("favorites/", TemplateView.as_view(template_name="diary/favorites.html"), name="favorites"),
 ]
